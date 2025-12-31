db14f537b3bb09fa36d3447191e0d383f989358b
Mahfuz Rahman <legitpixel626@gmail.com>
2026-01-01 00:17:44 +0530

    Update App.jsx

--- reactjs/src/App.jsx
+++ reactjs/src/App.jsx
@@ -1,11 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
    return (
     <>
+<div className='header'>
+<h1> Hey </h1> </div>
     </>
   )
 }
