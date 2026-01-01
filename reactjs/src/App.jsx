8fd80766864b7d22a48eddbae757faf1ae98a923
Mahfuz Rahman <legitpixel626@gmail.com>
2026-01-01 08:49:32 +0530

    Update App.jsx

--- reactjs/src/App.jsx
+++ reactjs/src/App.jsx
@@ -1,11 +1,8 @@
 import { useState } from 'react'
-import './index.css'
-import Card from './components/card.js'
 
 function App() {
    return (
-     <div className="header">
-       <div className="container"></div>
+     Hi
      </div>
   )
 }
